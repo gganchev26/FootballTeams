814f555441d4817d1b410f3ae7cc8812bb2f2118
Gancho <Gancho@DESKTOP-N082CBS>
2025-01-29 14:29:48 +0200

    Controllers

---
+++ FootballTeams/Controllers/BusinessController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FootballTeams.BL.Interfaces;
+using FootballTeams.Models.DTO;
+using FootballTeams.Models.Requests;
+
+
+namespace FootballTeams.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BusinessController : ControllerBase
+    {
+        private readonly IBusinessService _teamService;
+        public BusinessController(IBusinessService teamService)
+        {
+            _teamService = teamService;
+        }
+
+        [HttpGet("GetAllDetailedTeams")]
+        public IActionResult GetAllDetailedTeams()
+        {
+            var result = _teamService.GetAllTeams();
+
+            if (result != null && result.Count > 0)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+        }
+
+        [HttpPost("Test")]
+        public IActionResult Test([FromBody] TestRequest teams) 
+        {
+            return Ok();
+        }
+    }
+    public class TestRequest
+    {
+        public int MagicNumber { get; set; }
+        public string Description { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ FootballTeams/Controllers/TeamsController.cs
@@ -0,0 +1,70 @@
+﻿using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+using FootballTeams.BL.Interfaces;
+using FootballTeams.Models.DTO;
+using FootballTeams.Models.Requests;
+
+
+namespace FootballTeams.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeamsController : ControllerBase
+    {
+        private readonly ITeamsService _teamsService;
+        private readonly IMapper _mapper;
+
+        public TeamsController(ITeamsService teamservice, IMapper mapper)
+        {
+            _teamsService = teamservice;
+            _mapper = mapper;
+        }
+        
+        [HttpGet("GetAll")]
+        public IActionResult GetAll() 
+        {
+        var result = _teamsService.GetAll();
+
+            if (result != null && result.Count > 0) {
+                return Ok(result);
+            }
+            return NotFound();
+        }
+
+        [ProducesResponseType(StatusCode.Status200OK)]
+        [ProducesResponseType(StatusCode.Status404NotFound)]
+        [ProducesResponseType(StatusCode.Status400BadRequest)]
+        [HttpGet("GetById")]
+        public IActionResult GetById(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest($"Wrong ID: {id}");
+            }
+            var result = _teamsService.GetById(id);
+            
+            if (result == null)
+            {
+                return NotFound($"Team with ID:{id} not found");
+            }
+            
+            return Ok(result);
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add([FromBody]AddTeamRequest team)
+        {
+            var teamDto = _mapper.Map<Teams>(team);
+
+            _teamsService.Add(teamDto);
+
+            return Ok();
+        }
+
+        [HttpDelete("Delete")]
+        public void Delete(int id)
+        {
+            //return _teamsService.GetById(id);
+        }
+    }
+}
