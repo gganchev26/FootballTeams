e5db8e93bd4e60f68082b5f1b6a4e46737fb24a9
Gancho <Gancho@DESKTOP-N082CBS>
2025-01-29 13:50:32 +0200

    mapster

---
+++ FootballTeams/MapConfig/MapsterConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Mapster;
+using FootballTeams.Models.DTO;
+using FootballTeams.Models.Requests;
+
+
+namespace FootballTeams.MapConfig
+{
+    public class MapsterConfiguration
+    {
+        public static void Configure()
+        {
+            TypeAdapterConfig<AddTeamRequest, Teams>
+                .NewConfig();
+        }
+    }
+}
