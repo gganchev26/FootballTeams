50c0888416156ac7d9ad4d2d1fb21fd57df5a47c
Gancho <Gancho@DESKTOP-N082CBS>
2025-01-30 16:08:03 +0200

    Program.cs

--- FootballTeams/Program.cs
+++ FootballTeams/Program.cs
@@ -1,25 +1,61 @@
-var builder = WebApplication.CreateBuilder(args);
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Mapster;
+using FootballTeams.BL;
+using FootballTeams.BL.Interfaces;
+using FootballTeams.DL;
+using FootballTeams.Models.Configurations;
+using FootballTeams.Validators;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace FootballTeam
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Add configurations
+            builder.Services.Configure<MongoDbConfiguration>(
+                builder.Configuration
+                .GetSection(nameof(MongoDbConfiguration)));
+
+            //Add services to the container.
+            builder.Services
+                .RegisterRepositoies()
+                .RegisterServices();
+
+            builder.Services.AddMapster();
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddValidatorsFromAssemblyContaining<TestValidator>();
+
+            builder.Services.AddFluentValidationAutoValidation();
+
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddHealthChecks();
+
+            var app = builder.Build();
+
+            app.MapHealthChecks("/healthz");
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+        }
+    }
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
